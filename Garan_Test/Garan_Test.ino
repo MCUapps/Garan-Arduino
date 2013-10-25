c95fa3e98553f15528ee31a60b97ae92c139fe28
Fox Chao <fox@mcuapps.com>
2013-10-25 16:32:59 +0800

    commands with variable parameters

--- Garan_Test/Garan_Test.ino
+++ Garan_Test/Garan_Test.ino
@@ -8,13 +8,11 @@
 
 #include <SoftwareSerial.h>
 
+#if !defined(__MSP430_CPU__)
 SoftwareSerial player(2, 3); // RX, TX
-
-void waitButton() {
-  delay(3000);
-}
-
-unsigned char commandBuffer[10];
+#else
+SoftwareSerial player(P1_4, P1_5); // RX, TX
+#endif
 
 const unsigned char SINGLE_PLAY[]        = {0x04, 0x01, 0x00, 0x00, 0x01};  // NH NL
 const unsigned char SEQUENCE_PLAY[]      = {0x04, 0x02, 0x00, 0x00, 0x02};  // NH NL
@@ -42,51 +40,144 @@ const unsigned char FEEDBACK_AT_END[]    = {0x03, 0xA0, 0x01, 0x01};
 const unsigned char NO_FEEDBACK_AT_END[] = {0x03, 0xA0, 0x01, 0x00};
 const unsigned char GET_VERSION[]        = {0x02, 0x80, 0x01};
 
+unsigned char commandBuff[13];
+
+inline void buildCommand(unsigned char len, unsigned char cmd) {
+  commandBuff[0] = len;
+  commandBuff[1] = cmd;
+}
+
+unsigned char *singlePlay(unsigned int number) {
+  buildCommand(0x04, 0x01);
+  commandBuff[3] = number >> 8;
+  commandBuff[4] = number;
+
+  return commandBuff;
+}
+
+unsigned char *sequencePlay(unsigned int number) {
+  buildCommand(0x04, 0x02);
+  commandBuff[3] = number >> 8;
+  commandBuff[4] = number;
+
+  return commandBuff;
+}
+
+unsigned char *singleLoopPlay(unsigned int number) {
+  buildCommand(0x04, 0x02);
+  commandBuff[3] = number >> 8;
+  commandBuff[4] = number;
+
+  return commandBuff;
+}
+
+unsigned char *singlePlayName(String name) {
+  name.toCharArray((char *)&commandBuff[3], name.length() + 1);
+  buildCommand(name.length() + 2, 4);
+
+  return commandBuff;
+}
+
+unsigned char *sequencePlayName(String name) {
+  name.toCharArray((char *)&commandBuff[3], name.length() + 1);
+  buildCommand(name.length() + 2, 5);
+
+  return commandBuff;
+}
+
+unsigned char *singleLoopName(String name) {
+  name.toCharArray((char *)&commandBuff[3], name.length() + 1);
+  buildCommand(name.length() + 2, 6);
+
+  return commandBuff;
+}
+
+unsigned char *setVolume(unsigned char volume) {
+  buildCommand(0x03, 0x0D);
+  commandBuff[3] = volume;
+
+  return commandBuff;
+}
+
 void sendCommand(unsigned char command[]) {
   unsigned char checksum = 0;
 
   player.write(0x24);
+  Serial.print(String(0x24, HEX) + ' ');
 
   for (unsigned int i = 0; i <= command[0]; i++) {
     player.write(command[i]);
+    Serial.print(String(command[i], HEX) + ' ');
     checksum ^= command[i];
   }
-
   player.write(checksum);
+  Serial.print(String(checksum, HEX) + '\n');
 }
 
 void setup() {
   //Initialize serial and wait for port to open:
-  Serial.begin(115200); 
+  Serial.begin(9600); 
+
+#if !defined(__MSP430_CPU__)
+  // wait for USB CDC serial port to connect.  Arduino Leonardo only
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
   }
-  Serial.println("Arduino is ready!");
+#endif
+  Serial.println("Console is ready!");
  
   player.begin(9600);
+
+  // this byte must be 0
+  commandBuff[2] = 0;
 } 
 
 void loop() {
   if (Serial.available()) {
     switch(Serial.read()) {
       case '1':
-        sendCommand((unsigned char *)GET_VERSION);
+        sendCommand(singlePlay(1));
         break;
       case '2':
-        sendCommand((unsigned char *)SEQUENCE_PLAY);
+        sendCommand(singlePlay(2));
         break;
       case '3':
-        sendCommand((unsigned char *)NEXT);
+        sendCommand(singlePlay(3));
         break;
       case '4':
+        sendCommand(singlePlayName("01.mp3"));
+        break;
+      case '5':
+        sendCommand(sequencePlayName("01.mp3"));
+        break;
+      case '6':
+        sendCommand(singleLoopName("01.mp3"));
+        break;
+      case '9':
+        sendCommand(setVolume(15));
+        break;
+      case '0':
+        sendCommand(setVolume(0));
+        break;      
+      case 'n':
+        sendCommand((unsigned char *)NEXT);
+        break;
+      case 'l':
+        sendCommand((unsigned char *)LAST);
+        break;
+      case 't':
         sendCommand((unsigned char *)GET_MUSIC_TOTALITY);
+        break;
+      case 'v':
+        sendCommand((unsigned char *)GET_VERSION);
         break;
       case ']':
         sendCommand((unsigned char *)VOLUME_ADD);
         break;
       case '[':
         sendCommand((unsigned char *)VOLUME_SUBSTRACT);
-        break;      
+        break; 
+      default:
+        Serial.println("This key maps to none.");     
     }
   }
 
